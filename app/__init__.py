6bde47a7bf61a195fbf2af95ecbaa8e7c06cb932
Abhishek Saud <96973862+Aviisaud@users.noreply.github.com>
2024-05-13 15:43:36 +0530

    Flash message added

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.secret_key = '763ret4765_&345'
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from app import app
 import qrcode
 import base64
@@ -11,8 +11,19 @@ def index():
 @app.route('/generate_qr_code', methods=['POST'])
 def generate_qr_code():
     data = request.form['data']
+    
+    # Check if the data is empty
+    if not data:
+        # Flash a message to prompt the user to enter a URL
+        flash('Please enter a URL.')
+        # Redirect the user back to the index page
+        return redirect(url_for('index'))
+
+    # Generate QR code
     qr = qrcode.make(data)
     buffered = BytesIO()
     qr.save(buffered, format="PNG")
     qr_img_bytes = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+    # Render the QR code template with the QR image data and the input data
     return render_template('qr_code.html', qr_img_data=qr_img_bytes, data=data)
